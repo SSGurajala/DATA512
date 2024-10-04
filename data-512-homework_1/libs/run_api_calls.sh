f46d2dd5ab0a9d5d48c170b073006d4e9d57358a
SSGurajala <sgura99@uw.edu>
2024-10-03 18:23:25 -0700

    first commit

---
+++ data-512-homework_1/libs/run_api_calls.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 api_request.py desktop /Users/sgurajal/Desktop/DATA512/data-512-homework_1/data/ rare-disease_monthly_desktop_2015070100-2024093000.json rare-disease_cleaned.AUG.2024.csv 
