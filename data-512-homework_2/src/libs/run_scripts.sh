8d59773d10fef724ad50b853dd43150afd93af11
SSGurajala <sgura99@uw.edu>
2024-10-14 20:29:24 -0700

    small run scripts change

--- data-512-homework_2/src/libs/run_scripts.sh
+++ data-512-homework_2/src/libs/run_scripts.sh
@@ -4,7 +4,7 @@
 cd ${local_machine_scripts_directory}
 
 #run page info api calls
-python3 page_info_api_request.py ${intermediate_data_directory} ${pol_csv_file_name} ${revid_csv_file_name}
+python3 page_info_api_request.py ${initial_data_directory} ${pol_csv_file_name} ${revid_out_csv_file_name}
 
 #run page info api calls
-python3 ores_api_request.py ${intermediate_data_directory} ${revid_csv_file_name} ${pol_revid_out_file_name}
+python3 ores_api_request.py ${intermediate_data_directory} ${revid_csv_out_file_name} ${pol_revid_out_file_name}
